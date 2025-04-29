f4260ae7875714297dcfe5aa8130d41652dcf281
choeihwa <chldlghk@gmail.com>
2025-04-29 16:20:47 +0900

    Add database setting and develop example.

---
+++ jjikeobang-server/src/main/java/com/jjikeobang/common/TypeMapperUtil.java
@@ -0,0 +1,12 @@
+package com.jjikeobang.common;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class TypeMapperUtil {
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    public static LocalDateTime stringToLocalDateTime(String str) {
+        return LocalDateTime.parse(str, DATE_TIME_FORMATTER);
+    }
+}
---
+++ jjikeobang-server/src/main/java/com/jjikeobang/member/controller/MemberController.java
@@ -0,0 +1,30 @@
+package com.jjikeobang.member.controller;
+
+import com.jjikeobang.member.model.Member;
+import com.jjikeobang.member.service.MemberServiceImpl;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/member")
+public class MemberController extends HttpServlet {
+
+    private final MemberServiceImpl memberService;
+
+    public MemberController() {
+        this.memberService = new MemberServiceImpl();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<Member> members = memberService.selectAllMember();
+        for (Member member : members) {
+            resp.getWriter().println(member);
+        }
+    }
+}
