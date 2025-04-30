ca1b9f31e8a5e2878c55d6a19687562cbb805af8
ufo9430 <ufo9430@naver.com>
2025-04-30 17:49:54 +0900

    회원가입 조건 충족 시 저장 서비스 호출하는 기능 구현

--- jjikeobang-server/src/main/java/com/jjikeobang/member/controller/MemberJoinController.java
+++ jjikeobang-server/src/main/java/com/jjikeobang/member/controller/MemberJoinController.java
@@ -1,6 +1,8 @@
 package com.jjikeobang.member.controller;
 
 import com.jjikeobang.member.model.JoinMemberDTO;
+import com.jjikeobang.member.service.MemberService;
+import com.jjikeobang.member.service.MemberServiceImpl;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -11,6 +13,8 @@ import java.io.IOException;
 
 @WebServlet("/member/join")
 public class MemberJoinController extends HttpServlet {
+    private final MemberService memberService = new MemberServiceImpl();
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
@@ -18,18 +22,20 @@ public class MemberJoinController extends HttpServlet {
         String name = req.getParameter("name");
         String userPw = req.getParameter("userPw");
         String comparePw = req.getParameter("comparePw");
+
         // 비밀번호 불일치
         if (!userPw.equals(comparePw)) {
             printAlert(resp, "비밀번호가 일치하지 않습니다.");
             return;
         }
         // 아이디 중복 통과 X
-        if(true){
+        if(memberService.isDuplicatedId(userId)){
             printAlert(resp, "아이디를 확인해 주세요.");
             return;
         }
 
-        
+        memberService.putMember(new JoinMemberDTO(userId,userPw,name));
+
     }
 
     private static void printAlert(HttpServletResponse resp, String message) throws IOException {
