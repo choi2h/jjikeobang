d18d42ce44b45991791b9e4f88c97c05ba4d343a
choeihwa <chldlghk@gmail.com>
2025-05-01 15:27:58 +0900

    Remove incorrect code.

--- jjikeobang-server/src/main/java/com/jjikeobang/member/controller/MemberController.java
+++ jjikeobang-server/src/main/java/com/jjikeobang/member/controller/MemberController.java
@@ -2,8 +2,6 @@ package com.jjikeobang.member.controller;
 
 import com.jjikeobang.member.model.Member;
 import com.jjikeobang.member.service.MemberServiceImpl;
-import com.jjikeobang.room.model.Room;
-import com.jjikeobang.util.JsonUtil;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -17,35 +15,16 @@ import java.util.List;
 public class MemberController extends HttpServlet {
 
     private final MemberServiceImpl memberService;
-    private final JsonUtil jsonUtil;
 
     public MemberController() {
         this.memberService = new MemberServiceImpl();
-        this.jsonUtil = JsonUtil.getInstance();
     }
 
     @Override
-    public void init() throws ServletException {
-        System.out.println("@@@@@@@@@@@ - Init member controller.");
-    }
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         List<Member> members = memberService.selectAllMember();
         for (Member member : members) {
             resp.getWriter().println(member);
         }
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("Post member controller." + req.toString());
-        Room room = null;
-        try {
-             room = jsonUtil.getObjectFromJson(req.getReader(), Room.class);
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-        System.out.println(room.toString());
     }
 }
