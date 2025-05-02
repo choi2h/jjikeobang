7d1f03c872f0596efcaf1bbe7472a7b0cd342d8d
ufo9430 <ufo9430@naver.com>
2025-05-02 09:50:23 +0900

    로그인 세션 확인 후 다른 대시보드를 출력하는 인터페이스 구현

---
+++ jjikeobang-client/src/components/dashboard/UserDashboard.jsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useState, useNavigate } from "react";
+
+function GuestDashboard() {
+    const [name, setUsername] = useState(null);
+    const navigate = useNavigate();
+    const handleVoting = async() => {
+        navigate("/voting");
+    };
+
+    useEffect(() => {
+        fetch('/profile', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include',
+        }).then(res => {
+            if (res.ok) {
+                setUsername(res.name);
+            } else {
+                throw new Error('프로필 불러오기 실패');
+            }
+        })
+    });
+    
+    return (
+        <>
+        <div className="container-fluid main-container">
+
+            <div className="row mb-4">
+                <div className="col-12 d-flex justify-content-end">
+                    <div className="dropdown profile-dropdown">
+                        <button className="btn profile-btn dropdown-toggle" type="button" id="profileDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+                            <div className="profile-circle">
+                                <span value={name.charAt(0)}></span>
+                            </div>
+                            <span className="profile-name" value={name}>Guest</span>
+                        </button>
+                        <ul className="dropdown-menu" aria-labelledby="profileDropdown">
+                            <li><a className="dropdown-item" href="/history">지난 투표 기록</a></li>
+                            <li><a className="dropdown-item" href="/logout">로그아웃</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div className="row justify-content-center">
+                <div className="col-md-8 col-lg-6 text-center">
+                    <div className="logo-container mb-4">
+                        <img src="/assets/img/logo.png" alt="찍어방 로고" className="img-fluid logo-main" />
+                    </div>
+                    <div className="welcome-text mb-5">
+                        <p>새로운 방을 만들거나 참여하세요</p>
+                    </div>
+                    <div className="mb-4">
+                        <a href="/createRoom" className="btn btn-login btn-makeroom mb-3">방 만들기</a>
+                    </div>
+                    <div className="mb-5">
+                        <p className="text-center sub-text mb-2">또는 입장 코드로 참여</p>
+                        <div className="input-group room-code-input">
+                            <input type="text" className="form-control" placeholder="입장 코드" aria-label="입장 코드" />
+                            <button className="btn btn-primary" type="button" onClick={handleVoting}>입장</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="footer-text">
+                <p>멋쟁이사자처럼 백엔드 15기 회고 8팀</p>;
+            </div>
+        </div>
+        </>
+    );
+}
+
+
+export default GuestDashboard;
--- jjikeobang-client/src/pages/Index.js
+++ jjikeobang-client/src/pages/Index.js
@@ -1,31 +1,25 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import logo from '../assets/img/logo.png';
+import React, { useEffect } from 'react';
+import GuestDashboard from "../components/dashboard/GuestDashboard";
+import UserDashboard from "../components/dashboard/UserDashboard";
+
 
 function Main() {
-    return (
-    <div className="container-fluid main-container d-flex flex-column justify-content-center align-items-center">
-        <div className="logo-container text-center mb-3">
-            <img src={logo} alt="찍어방 로고" className="img-fluid logo-main" />
-        </div>
-        <div className="welcome-text text-center mb-4">
-            <p>새로운 방을 만들거나 참여하세요</p>
-        </div>
-        <div className="login-container mb-3">
-            <nav>
-                <Link to="/login" className="btn btn-login login-index">로그인</Link>
-            </nav>
-        </div>
-        <div className="signup-container mb-5">
-        <nav>
-            <Link to="/signup" className="signup-link">회원가입</Link>
-        </nav>
-        </div>
-        <div className="footer-text text-center">
-            <p>멋쟁이사자처럼 백엔드 15기 회고 8팀</p>
-        </div>
-    </div>
-    );
+    useEffect(() => {
+        fetch('/session', { credentials: 'include' })
+            .then(res => res.ok ? res.json() : Promise.reject())
+            .then(data => {
+                console.log('로그인 확인', data.memberId);
+                if (data.user) {
+                    return (<UserDashboard user={data.memberId} />);
+                } else {
+                    return (<GuestDashboard />);
+                }
+            })
+            .catch(() => {
+                console.log('로그인 안됨');
+                return (<GuestDashboard />);
+            });
+    })
 }
 
 export default Main;
